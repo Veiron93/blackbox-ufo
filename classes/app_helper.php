58c14b568288df8aa2aec3814fc9e8ce4d9b3366
Veiron93 <yudin9302@gmail.com>
2022-01-11 01:42:35 +1100

    правки слайдер на главной

--- classes/app_helper.php
+++ classes/app_helper.php
@@ -19,10 +19,11 @@ class App_Helper{
 	 }
 
 	public static function getBanners($category){
-		$result = Db_DbHelper::objectArray("SELECT b.id, b.name, b.description, b.link, b.banner_text, banner_type, bg_color,
+		$result = Db_DbHelper::objectArray("SELECT b.id, b.name, b.description, b.link, b.banner_text, b.link_type, b.link_page_id, tb.code as page_code, banner_type, bg_color,
 			image_width, image_height,
 			f.id as image_id, f.disk_name as image_path, b.open_in_new_window
 		FROM banners as b
+		LEFT JOIN admin_text_blocks as tb on tb.id = b.link_page_id
 		LEFT JOIN banners_categories as cat on cat.id = b.category_id
 		LEFT JOIN db_files as f on f.master_object_id = b.id and f.master_object_class ='Banners_Banner'
 		WHERE b.enabled = 1 AND cat.code = '{$category}'
