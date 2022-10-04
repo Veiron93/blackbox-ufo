07a0a2f8d1eca2fa3c1c88f3a249b3c56ad2be10
Veiron93 <yudin9302@gmail.com>
2022-10-04 18:36:20 +1100

    расширение модуля Shop. Добавлен список товаров с ссылками на них в каталоге

--- modules/blackbox/classes/blackbox_module.php
+++ modules/blackbox/classes/blackbox_module.php
@@ -21,24 +21,25 @@ class Blackbox_Module extends Core_ModuleBase
             if ($model instanceof Catalog_Product) {
                 $model->defineColumn("regular_photo", "Обычное фото");
                 $model->defineColumn("sales", "Продано");
-                $model->defineColumn("title_sku", "Заголовок у артикулов");
-                $model->defineColumn("is_sale", "Добавить в блок - Товары со скидкой");
+                $model->defineColumn("title_sku", "Заголовок у артикулов")->invisible();
+                $model->defineColumn("is_sale", "Добавить в блок - Товары со скидкой")->invisible();
 
                 // расширение для Б/У товаров
-                $model->defineColumn("is_useded_device", "Товар Б/У");
-                $model->defineColumn("show_block_useded_device", "Добавить в блок на главной");
-                $model->defineColumn("state_device_useded_device", "Состояние устройства");
-                $model->defineColumn("state_battery_useded_device", "Состояние аккумулятора");
-                $model->defineColumn("guarantee_useded_devicet", "Гарантия");
-                $model->defineColumn("defect_screen_useded_device", "Царапины на экране");
-                $model->defineColumn("defect_body_useded_device", "Царапины на корпусе");
-                $model->defineColumn("complect_useded_device", "Полный комплект");
-                $model->defineColumn("complect_non_elements_useded_device", "Отсутствующие предметы");
-                $model->defineColumn("added_acsessuares_useded_device", "Дополнительные аксессуары");
+                $model->defineColumn("is_useded_device", "Товар Б/У")->invisible();
+                $model->defineColumn("show_block_useded_device", "Добавить в блок на главной")->invisible();
+                $model->defineColumn("state_device_useded_device", "Состояние устройства")->invisible();
+                $model->defineColumn("state_battery_useded_device", "Состояние аккумулятора")->invisible();
+                $model->defineColumn("guarantee_useded_devicet", "Гарантия")->invisible();
+                $model->defineColumn("defect_screen_useded_device", "Царапины на экране")->invisible();
+                $model->defineColumn("defect_body_useded_device", "Царапины на корпусе")->invisible();
+                $model->defineColumn("complect_useded_device", "Полный комплект")->invisible();
+                $model->defineColumn("complect_non_elements_useded_device", "Отсутствующие предметы")->invisible();
+                $model->defineColumn("added_acsessuares_useded_device", "Дополнительные аксессуары")->invisible();
             }
         });
 
         Phpr::$events->addEvent(Db_Events::onModelDefineFormFields, function (Db_ActiveRecord $model) {
+
             if ($model instanceof Catalog_Category) {
                 $model->addFormField("hot", "left")->sortOrder(50)->tab(Catalog_Category::$generalTabTitle);
                 $model->addFormField("title_sku", "left")->sortOrder(20)->tab("Дополнительно");
@@ -63,6 +64,10 @@ class Blackbox_Module extends Core_ModuleBase
                 $model->addFormField("complect_useded_device", "left")->sortOrder(80)->tab($tab);
                 $model->addFormField("complect_non_elements_useded_device", "left")->sortOrder(90)->tab($tab);
                 $model->addFormField("added_acsessuares_useded_device", "left")->sortOrder(100)->tab($tab);
+            }
+
+            if ($model instanceof Shop_Order) {
+                $model->addFormPartial("modules/blackbox/controllers/partials/_list_goods.htm")->tab('Список товаров');
             }
         });
     }
