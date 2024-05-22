185a38f4439f6d9cf67e14652c3e4aadc3629f2c
Veiron93 <yudin9302@gmail.com>
2024-05-22 11:44:02 +1100

    slider portfolio protection

--- resources/dist/common-protection.min.js
+++ resources/dist/common-protection.min.js
@@ -2,6 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
 	window.cart = new Cart()
 	window.services = new Services()
 
+	new Swiper('.portfolio-slider-wrapper', {
+		loop: true,
+
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+
+		breakpoints: {
+			0: {
+				slidesPerView: 1.2,
+				spaceBetween: 10,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			991: {
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+		},
+	})
+
 	new Swiper('.brands_slider', {
 		loop: true,
 
---
+++ updates/2.sql
@@ -0,0 +1,4 @@
+begin;
+	insert into banners_categories (name, code)
+	values  ('Портфолио - Защита устройств', 'protection_portfolio');
+commit;
